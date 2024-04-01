e9b0c0caf6863c8e7b32191cc5b0f20ab535b7fb
victorr0909 <namit09092001@gmail.com>
2024-04-01 15:24:30 +0530

    Converting the code into class

--- snake-game.py
+++ snake-game.py
@@ -1,27 +1,24 @@
 from turtle import Turtle, Screen
+import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height = 600)
 screen.bgcolor("black")
 screen.title("My snake game")
+screen.tracer(0)
 
+snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-starting_positions = [(0,0),(-20,0),(-40,0)]
-
-for positions in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-
-
-
-
-
-
-
-
-
-
-
-
-screen.exitonclick()
+game_is_on = True
+while game_is_on:
+    screen.update()    
+    time.sleep(0.1)
+    
+    snake.move()
