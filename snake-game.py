40eef1d7bc93b7382c7ce04afd05fb92281a95fb
victorr0909 <namit09092001@gmail.com>
2024-04-01 12:41:55 +0530

    Adding dimension, Creating snake

---
+++ snake-game.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height = 600)
+screen.bgcolor("black")
+screen.title("My snake game")
+
+
+
+starting_positions = [(0,0),(-20,0),(-40,0)]
+
+for positions in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
